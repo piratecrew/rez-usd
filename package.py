c422d3026f40aa335f1ce94114a5c88c0dcdcc8d
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2019-06-20 15:13:59 +0200

    Add plugin env setup

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'usd'
 
-version = 'bundled-19.07'
+version = '19.07'
 
 authors = ['frbr']
 
@@ -13,36 +13,34 @@ build_requires = [
     "devtoolset-7",
 ]
 
-variants = [["platform-linux"]]
+variants = [
+    ["platform-linux"],
+    ["platform-linux", "houdini-17.0"],
+    ["platform-linux", "houdini-17.5"],
+    ["platform-linux", "maya-2018"],
+    ["platform-linux", "maya-2019"]
+]
 
-build_command = """
-if [[ $REZ_BUILD_INSTALL -eq 1 ]];
-then
-    mkdir -p $REZ_BUILD_INSTALL_PATH;
-    # Create Virtualenv
-    virtualenv --python=python2 $REZ_BUILD_INSTALL_PATH/.venv
-    source $REZ_BUILD_INSTALL_PATH/.venv/bin/activate
-    pip install PyOpenGL PySide2
-    # Clone USD
-    git clone https://github.com/PixarAnimationStudios/USD.git USD
-    cd USD
-    #git checkout v$REZ_BUILD_PROJECT_VERSION
-    git checkout v19.07
-    # Build USD
-    python build_scripts/build_usd.py $REZ_BUILD_INSTALL_PATH\
-        --src $REZ_BUILD_SOURCE_PATH/build/DEP_SOURCE\
-        --build $REZ_BUILD_SOURCE_PATH/build/DEP_BUILD\
-        --inst $REZ_BUILD_INSTALL_PATH/dependencies\
-        --openimageio --opencolorio --ptex\
-        --alembic --no-hdf5\
-        --materialx\
-        --build-args OpenImageIO,-DOpenGL_GL_PREFERENCE=GLVND OpenColorIO,"-DCMAKE_CXX_FLAGS=-w"
-  else
-    echo not running rez-install;
-fi
-"""
+build_command = "bash $REZ_BUILD_SOURCE_PATH/build_usd.sh"
 
 def commands():
-    env.PATH.prepend("{root}/.venv/bin")
-    env.PYTHONPATH.append("{root}/lib/python")
-    env.PATH.prepend("{root}/bin")
+    env.PATH.prepend("{base}/.venv/bin")
+    env.PYTHONPATH.append("{base}/USD-Core/lib/python")
+    env.PATH.prepend("{base}/USD-Core/bin")
+  
+    if "maya" in resolve:
+        env.MAYA_PLUG_IN_PATH.append('{root}/third_party/maya/plugin')
+        env.MAYA_SCRIPT_PATH.append('{root}/third_party/maya/lib/usd/usdMaya/resources')
+        env.MAYA_SCRIPT_PATH.append('{root}/third_party/maya/plugin/pxrUsdPreviewSurface/resources')
+        env.PYTHONPATH.append('{root}/lib/python')
+        env.XBMLANGPATH.append('{root}/third_party/maya/lib/usd/usdMaya/resources')
+   
+    if "houdini" in resolve:
+        env.HOUDINI_PATH.append('{root}/third_party/houdini')
+        env.HOUDINI_DSO_ERROR = 1
+        env.HOUDINI_DSO_PATH.append('@/plugin')
+        env.HOUDINI_DSO_PATH.append('&')
+        env.HOUDINI_SCRIPT_PATH.append('@/scripts')
+        env.HOUDINI_SCRIPT_PATH.append('{root}/lib')
+        env.HOUDINI_SCRIPT_PATH.append('&')
+        
